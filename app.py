47c746088cfdc8fe3d399b9daf7cde9d7e63214e
mlanorero <mlanorero@gmail.com>
2021-12-22 18:18:40 +0000

    commit

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from models import db, User, Post
 from flask_migrate import Migrate
 
-app = (__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:811t121l@localhost:5432/postgres'
-app.congin['SQLALCHEMY_TRACK_MODIFICATION']=False
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:contraseña@localhost:5432/postgres'
+app.congig['SQLALCHEMY_TRACK_MODIFICATION']=False
 app.config['DEBUG']=True
 
 db.init_app=(app)
